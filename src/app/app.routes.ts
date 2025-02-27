949dc0b95b06c17f542f806427bdea921e84e40a
Learn Smart Coding <learnsmartcoding@users.noreply.github.com>
2025-02-27 05:35:15 -0500

    code for ep 13 added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,20 @@ import { HomeComponent } from './components/home/home.component';
 import { DataBindingsComponent } from './components/data-bindings/data-bindings.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { CoursesComponent } from './components/courses/courses.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   {
     path: 'data-bindings',
-    loadComponent:()=> import('./components/data-bindings/data-bindings.component')
-    .then(m => m.DataBindingsComponent),
+    loadComponent: () =>
+      import('./components/data-bindings/data-bindings.component').then(
+        (m) => m.DataBindingsComponent
+      ),
   },
   {
-    path: 'about',    
+    path: 'about',
     loadComponent: () =>
       import('./pages/about/about.component').then((m) => m.AboutComponent),
   },
@@ -23,6 +26,10 @@ export const routes: Routes = [
       import('./pages/contact-us/contact-us.component').then(
         (m) => m.ContactUsComponent
       ),
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent,
   },
   { path: '**', redirectTo: 'home' },
 ];
